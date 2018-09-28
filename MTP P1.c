c5636f4fcaadd926398d2e92dc1d476ca80c3d32
MatheusmendesUFU <42947577+MatheusmendesUFU@users.noreply.github.com>
2018-09-27 22:23:54 -0300

    Add files via upload

---
+++ MTP P1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+	int estado = 0, i = 0;
+	char bits[256];
+	scanf("%s",bits);
+	for(i = 0; bits[i] != '\0'; i++){
+	
+	if(estado == 0 && bits[i] == '0'){
+		estado = 0;
+	}
+	else if(estado == 0 && bits[i] == '1'){
+		estado = 1;
+	}
+	else if(estado == 1 && bits[i] == '1'){
+		estado = 0;
+	}
+	else if(estado == 1 && bits[i] == '0'){
+		estado = 2;
+	}
+	else if(estado == 2 && bits[i] == '1'){
+		estado = 2;
+	}
+	else if(estado == 2 && bits[i] == '0'){
+		estado = 1;
+	}
+}
+	printf("%s", bits);	
+if(estado == 0)
+		printf("\ne multiplo de 3");
+	else 
+	printf("\nnao e");
+	
+return 0;	
+}
